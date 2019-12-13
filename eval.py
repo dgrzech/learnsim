utils/diff_op.py: Fix Jacobian values at the borders of the velocity field
utils/diff_op.py: Fix Jacobian values at the borders of the velocity field
utils/registration.py: Code clean-up
utils/transformation.py: Code clean-up

model/loss.py: Add LCC loss

eval.py: Code clean-up

tests/tests_registration.py: Code clean-up
tests/tests_utils.py: Code clean-up

eval.py
@@ -105,7 +105,7 @@ def main(config):
 
             for _ in range(no_samples):
                 v_sample = sample_qv(mu_v, log_var_v, u_v)
-                warp_field = transformation_model.forward_3d_add(identity_grid, v_sample)
+                warp_field = transformation_model.forward_3d(identity_grid, v_sample)
 
                 im_moving_warped = registration_module(im_moving, warp_field)
                 im_out = enc(im_fixed, im_moving_warped)
@@ -133,7 +133,7 @@ def main(config):
             """
 
             with torch.no_grad():
-                warp_field = transformation_model.forward_3d_add(identity_grid, mu_v)
+                warp_field = transformation_model.forward_3d(identity_grid, mu_v)
                 im_moving_warped = registration_module(im_moving, warp_field)
 
                 file_path = os.path.join(data_loader.save_dirs['im_moving_warped'],
@@ -153,7 +153,7 @@ def main(config):
     """
 
     with torch.no_grad():
-        warp_field = transformation_model.forward_3d_add(identity_grid, mu_v)
+        warp_field = transformation_model.forward_3d(identity_grid, mu_v)
         im_moving_warped = registration_module(im_moving, warp_field)
         im_out = enc(im_fixed, im_moving_warped)
 

model/loss.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod, ABC
 from torch import nn
+
 from utils.diff_op import GradientOperator
 
 import torch
@@ -12,9 +13,14 @@ data loss
 class DataLoss(nn.Module, ABC):
     def __init__(self):
         super(DataLoss, self).__init__()
-
-    def forward(self, z):
-        return self.reduce(z)
+    
+    @abstractmethod
+    def forward(self):
+        pass
+    
+    @abstractmethod
+    def map(self, im_fixed, im_moving):
+        pass
 
     @abstractmethod
     def reduce(self, z):
@@ -25,8 +31,31 @@ class SSD(DataLoss):
     def __init__(self):
         super(SSD, self).__init__()
 
+    def forward(self, z):
+        return self.reduce(z)
+
+    def map(self, im_fixed, im_moving):
+        return im_fixed - im_moving
+
     def reduce(self, z):
-        return 0.5 * torch.sum(z ** 2)
+        return 0.5 * torch.sum(torch.pow(z, 2))
+
+
+class LCC(DataLoss):
+    def __init__(self):
+        super(LCC, self).__init__()
+    
+    def forward(self, im_fixed, im_moving):
+        return self.reduce(im_fixed, im_moving)
+
+    def map(self, im_fixed, im_moving):
+        pass
+
+    def reduce(self, im_fixed, im_moving):
+        vx = im_fixed - torch.mean(im_fixed)
+        vy = im_moving - torch.mean(im_moving)
+
+        return torch.sum(vx * vy) / (torch.sqrt(torch.sum(torch.pow(vx, 2))) * torch.sqrt(torch.sum(torch.pow(vy, 2))))
 
 
 """
@@ -54,7 +83,7 @@ class RegLossL2(RegLoss):
 
     def forward(self, v):
         dv_dx, dv_dy, dv_dz = self.diff_op.apply(v)
-        return 0.5 * (torch.sum(dv_dx.pow(2)) + torch.sum(dv_dy.pow(2)) + torch.sum(dv_dz.pow(2)))
+        return 0.5 * (torch.sum(torch.pow(dv_dx, 2)) + torch.sum(torch.pow(dv_dy, 2)) + torch.sum(torch.pow(dv_dz, 2)))
 
 
 """
@@ -101,7 +130,7 @@ class KL(nn.Module):
         sigma_v = torch.exp(0.5 * log_var_v) + 1e-5
 
         t1 = 36.0 * torch.sum(sigma_v ** 2) \
-             + torch.sum(du_v_dx ** 2) + torch.sum(du_v_dy ** 2) + torch.sum(du_v_dz ** 2)
+             + torch.sum(torch.pow(du_v_dx, 2)) + torch.sum(torch.pow(du_v_dy, 2)) + torch.sum(torch.pow(du_v_dz, 2))
         t2 = torch.sum(dv_dx ** 2) + torch.sum(dv_dy ** 2) + torch.sum(dv_dz ** 2)
         t3 = -1.0 * (torch.log(1.0 + torch.sum(u_v * torch.pow(sigma_v, -2) * u_v)) + torch.sum(log_var_v))
 

tests/tests_registration.py
@@ -124,7 +124,7 @@ class RegistrationTestMethods(unittest.TestCase):
         total_loss = 0.0
 
         with torch.no_grad():
-            warp_field = self.transformation_model.forward_3d_add(self.identity_grid, self.mu_v)
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, self.mu_v)
             im_moving_warped = self.registration_module(im_moving, warp_field)
 
             print(f'PRE-REGISTRATION: ' +
@@ -145,7 +145,7 @@ class RegistrationTestMethods(unittest.TestCase):
 
             for _ in range(self.no_samples):
                 v_sample = sample_qv(self.mu_v, self.log_var_v, self.u_v)
-                warp_field = self.transformation_model.forward_3d_add(self.identity_grid, v_sample)
+                warp_field = self.transformation_model.forward_3d(self.identity_grid, v_sample)
 
                 im_moving_warped = self.registration_module(im_moving, warp_field)
                 im_out = self.enc(im_fixed, im_moving_warped)
@@ -188,7 +188,7 @@ class RegistrationTestMethods(unittest.TestCase):
         for _ in range(self.no_samples):
             # first term
             v_sample = sample_qv(self.mu_v, self.log_var_v, self.u_v)
-            warp_field = self.transformation_model.forward_3d_add(self.identity_grid, v_sample)
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, v_sample)
 
             im_moving_warped = self.registration_module(im_moving, warp_field)
             im_out = self.enc(im_fixed, im_moving_warped)
@@ -218,7 +218,7 @@ class RegistrationTestMethods(unittest.TestCase):
         """
 
         with torch.no_grad():
-            warp_field = self.transformation_model.forward_3d_add(self.identity_grid, self.mu_v)
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, self.mu_v)
             im_moving_warped = self.registration_module(im_moving, warp_field)
 
             save_im_to_disk(im_moving_warped, './temp/moving_warped.nii.gz')

tests/tests_utils.py
@@ -31,13 +31,11 @@ class UtilsTestMethods(unittest.TestCase):
         transformation = SVF()
 
         # v = torch.zeros(1, 2, self.dim_x, self.dim_y)
-        # warp_field = transformation.forward_2d_add(self.identity_grid_2d, v)
-        # warp_field = transformation.forward_2d_comp(self.identity_grid_2d, v)
+        # warp_field = transformation.forward_2d(self.identity_grid_2d, v)
         # print('zero velocity field\n', warp_field)
 
         v = 0.2 * torch.ones(1, 2, self.dim_x, self.dim_y)
-        warp_field = transformation.forward_2d_add(self.identity_grid_2d, v)
-        # warp_field = transformation.forward_2d_comp(self.identity_grid_2d, v)
+        warp_field = transformation.forward_2d(self.identity_grid_2d, v)
         # print('uniform velocity field\n', warp_field)
         plot_2d(v, warp_field)
 
@@ -45,13 +43,11 @@ class UtilsTestMethods(unittest.TestCase):
         transformation = SVF()
 
         # v = torch.zeros(1, 3, self.dim_x, self.dim_y, self.dim_z)
-        # warp_field = transformation.forward_3d_add(self.identity_grid_3d, v)
-        # warp_field = transformation.forward_3d_comp(self.identity_grid_3d, v)
+        # warp_field = transformation.forward_3d(self.identity_grid_3d, v)
         # print('zero velocity field\n', warp_field)
 
         v = 0.2 * torch.ones(1, 3, self.dim_x, self.dim_y, self.dim_z)
-        warp_field = transformation.forward_3d_add(self.identity_grid_3d, v)
-        # warp_field = transformation.forward_3d_comp(self.identity_grid_3d, v)
+        warp_field = transformation.forward_3d(self.identity_grid_3d, v)
         # print('uniform velocity field\n', warp_field)
         plot_3d(v, warp_field)
 
@@ -66,8 +62,7 @@ class UtilsTestMethods(unittest.TestCase):
                 v[0, 0, idx_x, idx_y] = y
                 v[0, 1, idx_x, idx_y] = -1.0 * x
 
-        warp_field = transformation.forward_2d_add(self.identity_grid_2d, v)
-        # warp_field = transformation.forward_2d_comp(self.identity_grid_2d, v)
+        warp_field = transformation.forward_2d(self.identity_grid_2d, v)
         # print(warp_field)
         plot_2d(v, warp_field)
 
@@ -84,7 +79,6 @@ class UtilsTestMethods(unittest.TestCase):
                     v[0, 1, idx_x, idx_y, idx_z] = -1.0 * x
                     v[0, 2, idx_x, idx_y, idx_z] = 0.0
 
-        warp_field = transformation.forward_3d_add(self.identity_grid_3d, v)
-        # warp_field = transformation.forward_3d_comp(self.identity_grid_3d, v)
+        warp_field = transformation.forward_3d(self.identity_grid_3d, v)
         # print(warp_field)
         plot_3d(v, warp_field)

utils/diff_op.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from torch import nn
 
-import torch
+import torch.nn.functional as F
 
 
 class DifferentialOperator(nn.Module, ABC):
@@ -17,22 +17,14 @@ class GradientOperator(DifferentialOperator):
     def __init__(self):
         super(GradientOperator, self).__init__()
 
+        self.px = (1, 1, 0, 0, 0, 0)
+        self.py = (0, 0, 1, 1, 0, 0)
+        self.pz = (0, 0, 0, 0, 1, 1)
+
     def apply(self, v):
-        dv_dx, dv_dy, dv_dz = torch.zeros_like(v), torch.zeros_like(v), torch.zeros_like(v)
-
-        # forward difference in the first entry
-        dv_dx[:, :, :, :, 0] = v[:, :, :, :, 1] - v[:, :, :, :, 0]
-        dv_dy[:, :, :, 0, :] = v[:, :, :, 1, :] - v[:, :, :, 0, :]
-        dv_dz[:, :, 0, :, :] = v[:, :, 1, :, :] - v[:, :, 0, :, :]
-
-        # backward difference in the last entry
-        dv_dx[:, :, :, :, -1] = v[:, :, :, :, -1] - v[:, :, :, :, -2]
-        dv_dy[:, :, :, -1, :] = v[:, :, :, -1, :] - v[:, :, :, -2, :]
-        dv_dz[:, :, -1, :, :] = v[:, :, -1, :, :] - v[:, :, -2, :, :]
-
-        # central differences elsewhere
-        dv_dx[:, :, :, :, 1:-1] = 0.5 * (v[:, :, :, :, 2:] - v[:, :, :, :, :-2])
-        dv_dy[:, :, :, 1:-1, :] = 0.5 * (v[:, :, :, 2:, :] - v[:, :, :, :-2, :])
-        dv_dz[:, :, 1:-1, :, :] = 0.5 * (v[:, :, 2:, :, :] - v[:, :, :-2, :, :])
+        # central differences
+        dv_dx = 0.5 * F.pad(v[:, :, :, :, 2:] - v[:, :, :, :, :-2], self.px, 'replicate')
+        dv_dy = 0.5 * F.pad(v[:, :, :, 2:, :] - v[:, :, :, :-2, :], self.py, 'replicate')
+        dv_dz = 0.5 * F.pad(v[:, :, 2:, :, :] - v[:, :, :-2, :, :], self.pz, 'replicate')
 
         return dv_dx, dv_dy, dv_dz

utils/registration.py
@@ -7,6 +7,6 @@ class RegistrationModule(nn.Module):
     def __init__(self):
         super(RegistrationModule, self).__init__()
 
-    def forward(self, m, warp_field):
+    def forward(self, im_moving, warp_field):
         grid = warp_field.permute([0, 2, 3, 4, 1])
-        return F.grid_sample(m, grid, padding_mode='border')
+        return F.grid_sample(im_moving, grid, padding_mode='border')

utils/transformation.py
@@ -13,19 +13,11 @@ class TransformationModel(nn.Module, ABC):
         super(TransformationModel, self).__init__()
 
     @abstractmethod
-    def forward_2d_add(self, identity_grid, v):
+    def forward_2d(self, identity_grid, v):
         pass
 
     @abstractmethod
-    def forward_2d_comp(self, identity_grid, v):
-        pass
-
-    @abstractmethod
-    def forward_3d_add(self, identity_grid, v):
-        pass
-
-    @abstractmethod
-    def forward_3d_comp(self, identity_grid, v):
+    def forward_3d(self, identity_grid, v):
         pass
 
 
@@ -36,9 +28,9 @@ class SVF(TransformationModel):
 
     def __init__(self):
         super(SVF, self).__init__()
-        self.no_steps = 12
+        self.no_steps = 16
 
-    def forward_2d_add(self, identity_grid, v):
+    def forward_2d(self, identity_grid, v):
         warp_field = v / float(2 ** self.no_steps)
 
         for _ in range(self.no_steps):
@@ -49,18 +41,7 @@ class SVF(TransformationModel):
 
         return identity_grid.permute([0, 3, 1, 2]) + warp_field
 
-    def forward_2d_comp(self, identity_grid, v):
-        warp_field = identity_grid.permute([0, 3, 1, 2]) + v / float(2 ** self.no_steps)
-
-        for _ in range(self.no_steps):
-            grid_sample_input = warp_field
-            grid = grid_sample_input.permute([0, 2, 3, 1])
-
-            warp_field = F.grid_sample(grid_sample_input, grid, padding_mode='border')
-
-        return warp_field
-
-    def forward_3d_add(self, identity_grid, v):
+    def forward_3d(self, identity_grid, v):
         warp_field = v / float(2 ** self.no_steps)
 
         for _ in range(self.no_steps):
@@ -70,14 +51,3 @@ class SVF(TransformationModel):
             warp_field = grid_sample_input + F.grid_sample(grid_sample_input, grid, padding_mode='border')
 
         return identity_grid.permute([0, 4, 1, 2, 3]) + warp_field
-
-    def forward_3d_comp(self, identity_grid, v):
-        warp_field = identity_grid.permute([0, 4, 1, 2, 3]) + v / float(2 ** self.no_steps)
-
-        for _ in range(self.no_steps):
-            grid_sample_input = warp_field
-            grid = grid_sample_input.permute([0, 2, 3, 4, 1])
-
-            warp_field = F.grid_sample(grid_sample_input, grid, padding_mode='border')
-
-        return warp_field