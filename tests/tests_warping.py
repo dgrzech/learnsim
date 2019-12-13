dfce64de350bac3f6d6607792111cb027be4bf08
dgrzech <d.grzech17@imperial.ac.uk>
2019-12-13 11:51:15 +0000

    tests/tests_warping.py: Add another warping test

--- tests/tests_warping.py
+++ tests/tests_warping.py
@@ -70,3 +70,36 @@ class WarpingTestMethods(unittest.TestCase):
 
         save_im_to_disk(im_moving, './temp/moving.nii.gz')
         save_im_to_disk(im_moving_warped, './temp/moving_warped.nii.gz')
+
+    def test_sphere_translation_large(self):
+        """
+        initialise 3D image of a sphere
+        """
+
+        im_moving = -1.0 + torch.zeros(self.dims_im).to('cuda:0')
+        r = 0.02
+
+        for idx_z in range(im_moving.shape[2]):
+            for idx_y in range(im_moving.shape[3]):
+                for idx_x in range(im_moving.shape[4]):
+                    x, y, z = pixel_to_normalised_3d(idx_x, idx_y, idx_z, self.dim_x, self.dim_y, self.dim_z)
+
+                    if x ** 2 + y ** 2 + z ** 2 <= r:
+                        im_moving[0, 0, idx_x, idx_y, idx_z] = 1.0
+
+        """
+        initialise a warp field
+        """
+
+        offset = 20
+
+        warp_field = self.identity_grid.permute([0, 4, 1, 2, 3]) + offset / self.dim_x * torch.ones(self.dims_v).to('cuda:0')
+        im_moving_warped = self.registration_module(im_moving, warp_field)
+
+        """"
+        save the images to disk
+        """
+
+        save_im_to_disk(im_moving, './temp/moving.nii.gz')
+        save_im_to_disk(im_moving_warped, './temp/moving_warped_large.nii.gz')
+
