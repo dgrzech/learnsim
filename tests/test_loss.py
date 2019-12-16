f3456edeb929f0cbc0619b989d840befb0a057b5
dgrzech <d.grzech17@imperial.ac.uk>
2019-12-16 13:31:30 +0000

    model/loss.py: Fix LCC loss
    tests/test_diff.py: Change names of test files
    tests/test_loss.py: Add tests for LCC
    tests/test_registration.py: Change names of test files
    tests/test_utils.py: Change names of test files
    tests/test_warping.py: Add test for warping an image from the test dataset

---
+++ tests/test_loss.py
@@ -0,0 +1,105 @@
+from model.loss import EntropyMultivariateNormal, LCC
+
+import math
+import numpy as np
+import pytest
+import torch
+import torch.nn.functional as F
+import unittest
+
+# fix random seeds for reproducibility
+SEED = 123
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+
+torch.autograd.set_detect_anomaly(True)
+
+
+class LossTestMethods(unittest.TestCase):
+    def setUp(self):
+        print(self._testMethodName)
+
+        n = 4
+
+        self.dim_x = n
+        self.dim_y = n
+        self.dim_z = n
+
+        self.s = 4
+        self.padding = (self.s, self.s, self.s, self.s, self.s, self.s)
+
+    def test_entropy(self):
+        # initialise the loss object
+        entropy = EntropyMultivariateNormal()
+
+        # initialise sigma_v
+        log_var_v = torch.log(torch.abs(torch.randn(self.dim_x, self.dim_y, self.dim_z)))
+        sigma_v = torch.exp(0.5 * log_var_v) + 1e-5
+        var_v = sigma_v ** 2
+
+        # initialise the first mode of variation
+        u_v = torch.zeros((self.dim_x, self.dim_y, self.dim_z))
+
+        # calculate the entropy
+        val = entropy.forward(log_var_v, u_v).item()
+        val_true = 0.5 * math.log(np.linalg.det(np.diag(var_v.cpu().numpy().flatten())))
+
+        assert pytest.approx(val, 0.01) == val_true
+
+    def test_lcc_uniform(self):
+        # initialise the images
+        im_fixed = torch.zeros(1, 1, self.dim_x, self.dim_y, self.dim_z).to('cuda:0')
+        im_moving = 2.0 * torch.ones(1, 1, self.dim_x, self.dim_y, self.dim_z).to('cuda:0')
+
+        # initialise the loss object
+        lcc = LCC(self.s)
+
+        # calculate the local means
+        im_fixed_padded = F.pad(im_fixed, self.padding, mode='replicate')
+        im_moving_padded = F.pad(im_moving, self.padding, mode='replicate')
+
+        u_F, u_M = lcc.compute_local_means(im_fixed_padded, im_moving_padded)
+
+        assert torch.all(torch.eq(u_F, im_fixed))
+        assert torch.all(torch.eq(u_M, im_moving))
+
+        # calculate the local sums
+        cross, var_F, var_M = lcc.compute_local_sums(im_fixed_padded, im_moving_padded)
+
+        zero_tensor = torch.zeros(1, 1, self.dim_x, self.dim_y, self.dim_z).to('cuda:0')
+        assert torch.all(torch.eq(cross, zero_tensor))
+        assert torch.all(torch.eq(var_F, zero_tensor))
+        assert torch.all(torch.eq(var_M, zero_tensor))
+
+        # calculate the value of loss
+        val = lcc(im_fixed, im_moving).item()
+        val_true = 0.0
+
+        assert pytest.approx(val, 0.01) == val_true
+
+    def test_lcc(self):
+        # initialise the images
+        im_fixed = torch.randn(1, 1, self.dim_x, self.dim_y, self.dim_z).to('cuda:0')
+        im_moving = 4.0 * im_fixed
+
+        # initialise the loss object
+        lcc = LCC(self.s)
+
+        # calculate the local means
+        im_fixed_padded = F.pad(im_fixed, self.padding, mode='replicate')
+        im_moving_padded = F.pad(im_moving, self.padding, mode='replicate')
+
+        u_F, u_M = lcc.compute_local_means(im_fixed_padded, im_moving_padded)
+        assert torch.all(torch.eq(4.0 * u_F, u_M))
+
+        # calculate the local sums
+        cross, var_F, var_M = lcc.compute_local_sums(im_fixed_padded, im_moving_padded)
+        assert torch.all(torch.eq(16.0 * var_F, var_M))
+
+        # calculate the value of loss
+        val = lcc(im_fixed, im_moving).item()
+        no_voxels = float(self.dim_x * self.dim_y * self.dim_z)
+
+        assert pytest.approx(val, 0.01) == -1.0 * no_voxels
--- tests/test_warping.py
+++ tests/test_warping.py
@@ -1,7 +1,11 @@
-from utils.registration import RegistrationModule
-from utils.util import init_identity_grid_3d, pixel_to_normalised_3d, save_im_to_disk
+from skimage import transform
 
+from utils.registration import RegistrationModule
+from utils.util import init_identity_grid_3d, pixel_to_normalised_3d, rescale_im, standardise_im, save_im_to_disk
+
+import math
 import numpy as np
+import SimpleITK as sitk
 import torch
 import unittest
 
@@ -103,3 +107,58 @@ class WarpingTestMethods(unittest.TestCase):
         save_im_to_disk(im_moving, './temp/moving.nii.gz')
         save_im_to_disk(im_moving_warped, './temp/moving_warped_large.nii.gz')
 
+    def test_brain_rotation(self):
+        """
+        initialise a rotation matrix
+        """
+
+        theta = math.pi / 2.0  # 90 degrees
+
+        R_arr = [[math.cos(theta), -1.0 * math.sin(theta), 0.0],
+                 [math.sin(theta), math.cos(theta), 0.0],
+                 [0.0, 0.0, 1.0]]
+        R = torch.Tensor(R_arr).to('cuda:0')
+
+        """
+        initialise a warp field
+        """
+
+        dim_x = 128
+        dim_y = 128
+        dim_z = 128
+
+        identity_grid = init_identity_grid_3d(dim_x, dim_y, dim_z).to('cuda:0')
+        warp_field = identity_grid.permute([0, 4, 1, 2, 3])
+
+        for idx_z in range(warp_field.shape[2]):
+            for idx_y in range(warp_field.shape[3]):
+                for idx_x in range(warp_field.shape[4]):
+                    p = warp_field[0, :, idx_z, idx_y, idx_x]
+
+                    p_new = torch.mv(R, p)
+                    warp_field[0, :, idx_z, idx_y, idx_x] = p_new
+
+        """
+        load image and warp it
+        """
+
+        im_path = '/vol/bitbucket/dig15/biobank_test/1007582_T2_FLAIR_unbiased_brain_affine_to_mni.nii.gz'
+        im_moving = sitk.ReadImage(im_path, sitk.sitkFloat32)
+
+        im_moving = torch.from_numpy(transform.resize(
+            np.transpose(sitk.GetArrayFromImage(im_moving), (2, 1, 0)), (dim_x, dim_y, dim_z)))
+
+        im_moving = standardise_im(im_moving)
+        im_moving = rescale_im(im_moving)
+
+        im_moving.unsqueeze_(0).unsqueeze_(0)
+        im_moving = im_moving.to('cuda:0')
+
+        im_moving_warped = self.registration_module(im_moving, warp_field)
+
+        """"
+        save the images to disk
+        """
+
+        save_im_to_disk(im_moving, './temp/brain_moving.nii.gz')
+        save_im_to_disk(im_moving_warped, './temp/brain_moving_warped.nii.gz')
