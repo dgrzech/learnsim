24a2c2c99256dd6d9111b9d88999cf224ebb4c06
dgrzech <d.grzech17@imperial.ac.uk>
2019-12-18 15:50:49 +0000

    tests/test_registration.py: Separate registration tests for SSD and LCC

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,14 +1,12 @@
-from mpl_toolkits.mplot3d import Axes3D
-from torch.optim import SGD
+from torch.optim import Adam, SGD
 
-from model.loss import SSD, RegLossL2, EntropyMultivariateNormal
+from model.loss import LCC, SSD, RegLossL2, EntropyMultivariateNormal
 from model.model import SimEnc
 from utils.registration import RegistrationModule
 from utils.sampler import sample_qf, sample_qv
 from utils.transformation import SVF
 from utils.util import init_identity_grid_3d, pixel_to_normalised_3d, save_im_to_disk
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import unittest
@@ -42,25 +40,12 @@ class RegistrationTestMethods(unittest.TestCase):
         self.identity_grid = init_identity_grid_3d(self.dim_x, self.dim_y, self.dim_z).to('cuda:0')
 
         """
-        parameters to learn
-        """
-
-        self.mu_v = torch.zeros(self.dims_v).to('cuda:0').requires_grad_(True)
-
-        var_v = float(self.dim_x ** (-2)) * torch.ones(self.dims_v)
-        self.log_var_v = torch.log(var_v).to('cuda:0').requires_grad_(True)
-        self.u_v = torch.zeros(self.dims_v).to('cuda:0').requires_grad_(True)
-
-        var_f = float(0.1 ** 2) * torch.ones(self.dims_im)
-        self.log_var_f = torch.log(var_f).to('cuda:0').requires_grad_(True)
-        self.u_f = torch.zeros(self.dims_im).to('cuda:0').requires_grad_(True)
-
-        """
         modules
         """
 
-        s = 7
-        self.enc = SimEnc(s).to('cuda:0')
+        self.s = 7
+        self.s_LCC = 5
+
         self.transformation_model = SVF().to('cuda:0')
         self.registration_module = RegistrationModule().to('cuda:0')
 
@@ -68,32 +53,23 @@ class RegistrationTestMethods(unittest.TestCase):
         losses
         """
 
-        self.data_loss = SSD().to('cuda:0')
         self.reg_loss = RegLossL2('GradientOperator').to('cuda:0')
         self.entropy = EntropyMultivariateNormal().to('cuda:0')
-
-        """
-        optimisers
-        """
-
-        self.optimizer_v = SGD([self.mu_v, self.log_var_v, self.u_v], lr=1e-3)
-        self.optimizer_f = SGD([self.log_var_f, self.u_f], lr=1e-3)
-        self.optimizer_phi = SGD(self.enc.parameters(), lr=1e-3)
 
         """
         optimiser parameters
         """
 
-        self.no_steps_v = 64
+        self.no_steps_v = 128
         self.no_samples = 6
 
-    def test_sphere_translation(self):
-        im_fixed = -1.0 + torch.zeros(self.dims_im).to('cuda:0')
-        im_moving = -1.0 + torch.zeros(self.dims_im).to('cuda:0')
-
+    def test_sphere_translation_LCC_data_only(self):
         """
         initialise 3D images of spheres
         """
+
+        im_fixed = -1.0 + torch.zeros(self.dims_im).to('cuda:0')
+        im_moving = -1.0 + torch.zeros(self.dims_im).to('cuda:0')
 
         r = 0.02
 
@@ -118,47 +94,163 @@ class RegistrationTestMethods(unittest.TestCase):
         save_im_to_disk(im_moving, './temp/moving.nii.gz')
 
         """
+        loss, parameters to learn, and optimiser
+        """
+
+        data_loss = LCC(self.s_LCC).to('cuda:0')
+        mu_v = torch.zeros(self.dims_v).to('cuda:0').requires_grad_(True)
+        optimizer_v = Adam([mu_v], lr=1e-3)
+
+        """
         registration
         """
 
         total_loss = 0.0
 
         with torch.no_grad():
-            warp_field = self.transformation_model.forward_3d(self.identity_grid, self.mu_v)
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, mu_v)
             im_moving_warped = self.registration_module(im_moving, warp_field)
 
             print(f'PRE-REGISTRATION: ' +
-                  f'{self.data_loss(im_fixed - im_moving).item():.2f}' +
-                  f', {self.data_loss(im_fixed - im_moving_warped).item():.2f}\n'
+                  f'{data_loss(im_fixed, im_moving).item():.2f}' +
+                  f', {data_loss(im_fixed, im_moving_warped).item():.2f}\n'
                   )
 
         """
         q_v
         """
 
-        self.enc.eval()
-        self.enc.set_grad_enabled(False)
+        for iter_no in range(self.no_steps_v):
+            optimizer_v.zero_grad()
+
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, mu_v)
+            im_moving_warped = self.registration_module(im_moving, warp_field)
+
+            data_term = data_loss(im_fixed, im_moving_warped)
+            reg_term = self.reg_loss(mu_v).sum()
+
+            loss_qv = data_term + reg_term
+            loss_qv.backward()
+            optimizer_v.step()
+
+            print(f'ITERATION ' + str(iter_no) + '/' + str(self.no_steps_v - 1) +
+                  f', TOTAL ENERGY: {loss_qv.item():.2f}' +
+                  f'\ndata: {data_term.item():.2f}' +
+                  f', regularisation: {reg_term.item():.2f}'
+                  )
+
+            total_loss += (loss_qv.item() / float(self.no_steps_v))
+
+        mu_v.requires_grad_(False)
+
+        """
+        save image of the warped sphere to disk
+        """
+
+        with torch.no_grad():
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, mu_v)
+            im_moving_warped = self.registration_module(im_moving, warp_field)
+
+            save_im_to_disk(im_moving_warped, './temp/moving_warped_LCC_data_only.nii.gz')
+
+    def test_sphere_translation_SSD_full(self):
+        """
+        initialise 3D images of spheres
+        """
+        im_fixed = -1.0 + torch.zeros(self.dims_im).to('cuda:0')
+        im_moving = -1.0 + torch.zeros(self.dims_im).to('cuda:0')
+
+        r = 0.02
+
+        offset_x = 5
+        offset_y = 5
+        offset_z = 5
+
+        for idx_z in range(im_fixed.shape[2]):
+            for idx_y in range(im_fixed.shape[3]):
+                for idx_x in range(im_fixed.shape[4]):
+                    x, y, z = pixel_to_normalised_3d(idx_x, idx_y, idx_z, self.dim_x, self.dim_y, self.dim_z)
+
+                    if x ** 2 + y ** 2 + z ** 2 <= r:
+                        im_fixed[0, 0, idx_x, idx_y, idx_z] = 1.0
+                        im_moving[0, 0, idx_x + offset_x, idx_y + offset_y, idx_z + offset_z] = 1.0
+
+        """
+        parameters to learn
+        """
+
+        mu_v = torch.zeros(self.dims_v).to('cuda:0').requires_grad_(True)
+
+        var_v = float(self.dim_x ** (-2)) * torch.ones(self.dims_v)
+        log_var_v = torch.log(var_v).to('cuda:0').requires_grad_(True)
+        u_v = torch.zeros(self.dims_v).to('cuda:0').requires_grad_(True)
+
+        var_f = float(0.1 ** 2) * torch.ones(self.dims_im)
+        log_var_f = torch.log(var_f).to('cuda:0').requires_grad_(True)
+        u_f = torch.zeros(self.dims_im).to('cuda:0').requires_grad_(True)
+
+        """
+        encoding function
+        """
+
+        enc = SimEnc(self.s).to('cuda:0')
+
+        """
+        data loss
+        """
+
+        data_loss = SSD().to('cuda:0')
+
+        """
+        optimisers
+        """
+
+        optimizer_v = SGD([mu_v, log_var_v, u_v], lr=1e-3)
+        optimizer_f = SGD([log_var_f, u_f], lr=1e-3)
+        optimizer_phi = SGD(enc.parameters(), lr=1e-3)
+
+        """
+        registration
+        """
+
+        total_loss = 0.0
+
+        with torch.no_grad():
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, mu_v)
+            im_moving_warped = self.registration_module(im_moving, warp_field)
+
+            print(f'PRE-REGISTRATION: ' +
+                  f'{data_loss(im_fixed - im_moving).item():.2f}' +
+                  f', {data_loss(im_fixed - im_moving_warped).item():.2f}\n'
+                  )
+
+        """
+        q_v
+        """
+
+        enc.eval()
+        enc.set_grad_enabled(False)
 
         for iter_no in range(self.no_steps_v):
-            self.optimizer_v.zero_grad()
+            optimizer_v.zero_grad()
             data_term = 0.0
 
             for _ in range(self.no_samples):
-                v_sample = sample_qv(self.mu_v, self.log_var_v, self.u_v)
+                v_sample = sample_qv(mu_v, log_var_v, u_v)
                 warp_field = self.transformation_model.forward_3d(self.identity_grid, v_sample)
 
                 im_moving_warped = self.registration_module(im_moving, warp_field)
-                im_out = self.enc(im_fixed, im_moving_warped)
+                im_out = enc(im_fixed, im_moving_warped)
 
-                data_term_sample = self.data_loss(im_out).sum() / float(self.no_samples)
+                data_term_sample = data_loss(im_out).sum() / float(self.no_samples)
                 data_term += data_term_sample
 
-            reg_term = self.reg_loss(self.mu_v).sum()
-            entropy_term = self.entropy(self.log_var_v, self.u_v).sum()
+            reg_term = self.reg_loss(mu_v).sum()
+            entropy_term = self.entropy(log_var_v, u_v).sum()
 
             loss_qv = data_term + reg_term - entropy_term
             loss_qv.backward()
-            self.optimizer_v.step()
+            optimizer_v.step()
 
             print(f'ITERATION ' + str(iter_no) + '/' + str(self.no_steps_v - 1) +
                   f', TOTAL ENERGY: {loss_qv.item():.2f}' +
@@ -169,56 +261,56 @@ class RegistrationTestMethods(unittest.TestCase):
 
             total_loss += (loss_qv.item() / float(self.no_steps_v))
 
-        self.mu_v.requires_grad_(False)
-        self.log_var_v.requires_grad_(False)
-        self.u_v.requires_grad_(False)
+        mu_v.requires_grad_(False)
+        log_var_v.requires_grad_(False)
+        u_v.requires_grad_(False)
 
         """
         q_phi
         """
 
-        self.enc.train()
-        self.enc.set_grad_enabled(True)
+        enc.train()
+        enc.set_grad_enabled(True)
 
-        self.optimizer_phi.zero_grad()
-        self.optimizer_f.zero_grad()
+        optimizer_phi.zero_grad()
+        optimizer_f.zero_grad()
 
         loss_qphi = 0.0
 
         for _ in range(self.no_samples):
             # first term
-            v_sample = sample_qv(self.mu_v, self.log_var_v, self.u_v)
+            v_sample = sample_qv(mu_v, log_var_v, u_v)
             warp_field = self.transformation_model.forward_3d(self.identity_grid, v_sample)
 
             im_moving_warped = self.registration_module(im_moving, warp_field)
-            im_out = self.enc(im_fixed, im_moving_warped)
+            im_out = enc(im_fixed, im_moving_warped)
 
-            data_term_sample = self.data_loss(im_out).sum() / float(self.no_samples)
+            data_term_sample = data_loss(im_out).sum() / float(self.no_samples)
             loss_qphi += data_term_sample
 
             # second term
             for _ in range(self.no_samples):
-                f_sample = sample_qf(im_fixed, self.log_var_f, self.u_f)
-                im_out = self.enc(f_sample, im_moving_warped)
+                f_sample = sample_qf(im_fixed, log_var_f, u_f)
+                im_out = enc(f_sample, im_moving_warped)
 
-                data_term_sample = self.data_loss(im_out).sum() / float(self.no_samples ** 2)
+                data_term_sample = data_loss(im_out).sum() / float(self.no_samples ** 2)
                 loss_qphi -= data_term_sample
 
         loss_qphi.backward()
-        self.optimizer_f.step()
-        self.optimizer_phi.step()
+        optimizer_f.step()
+        optimizer_phi.step()
 
         total_loss += loss_qphi.item()
 
-        self.log_var_f.requires_grad_(False)
-        self.u_f.requires_grad_(False)
+        log_var_f.requires_grad_(False)
+        u_f.requires_grad_(False)
 
         """
         save image of the warped sphere to disk
         """
 
         with torch.no_grad():
-            warp_field = self.transformation_model.forward_3d(self.identity_grid, self.mu_v)
+            warp_field = self.transformation_model.forward_3d(self.identity_grid, mu_v)
             im_moving_warped = self.registration_module(im_moving, warp_field)
 
-            save_im_to_disk(im_moving_warped, './temp/moving_warped.nii.gz')
+            save_im_to_disk(im_moving_warped, './temp/moving_warped_SSD_all.nii.gz')
