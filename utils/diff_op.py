c419680a8e2b8cf2b91eb1c2bfe72fbc0ee8e262
dgrzech <d.grzech17@imperial.ac.uk>
2020-02-17 17:21:23 +0000

    utils/diff_op.py: Fix bug in the regularisation term

--- utils/diff_op.py
+++ utils/diff_op.py
@@ -29,6 +29,7 @@ class GradientOperator(DifferentialOperator):
         self.pixel_spacing = None
 
     def forward(self, v):
+        # F.grid_sample(..) takes values in range (-1, 1), so needed for det(J) = 1 when the transformation is identity
         if self.pixel_spacing is None:
             dim_x = v.size()[4]
             dim_y = v.size()[3]
@@ -36,10 +37,14 @@ class GradientOperator(DifferentialOperator):
 
             self.pixel_spacing = (2.0 / float(dim_x - 1), 2.0 / float(dim_y - 1), 2.0 / float(dim_z - 1))
 
-        # central differences
-        dv_dx = F.pad(v[:, :, :, :, 2:] - v[:, :, :, :, :-2], self.px) / (2.0 * self.pixel_spacing[0])
-        dv_dy = F.pad(v[:, :, :, 2:, :] - v[:, :, :, :-2, :], self.py) / (2.0 * self.pixel_spacing[1])
-        dv_dz = F.pad(v[:, :, 2:, :, :] - v[:, :, :-2, :, :], self.pz) / (2.0 * self.pixel_spacing[2])
+        v_padded_x = F.pad(v, self.px, mode='replicate')
+        dv_dx = (v_padded_x[:, :, :, :, 2:] - v_padded_x[:, :, :, :, :-2]) / (2.0 * self.pixel_spacing[0])
+
+        v_padded_y = F.pad(v, self.py, mode='replicate')
+        dv_dy = (v_padded_y[:, :, :, 2:] - v_padded_y[:, :, :, :-2]) / (2.0 * self.pixel_spacing[1])
+
+        v_padded_z = F.pad(v, self.pz, mode='replicate')
+        dv_dz = (v_padded_z[:, :, 2:] - v_padded_z[:, :, :-2]) / (2.0 * self.pixel_spacing[2])
 
         nabla_vx = torch.stack((dv_dx[:, 0], dv_dy[:, 0], dv_dz[:, 0]), 1)
         nabla_vy = torch.stack((dv_dx[:, 1], dv_dy[:, 1], dv_dz[:, 1]), 1)
