3bc9f2cb7c68ee06eaa0475b9b26052f71a15f0a
dgrzech <d.grzech17@imperial.ac.uk>
2020-02-18 11:31:56 +0000

    utils/diff_op.py: Change differentiation scheme to forward differences

--- utils/diff_op.py
+++ utils/diff_op.py
@@ -22,14 +22,15 @@ class GradientOperator(DifferentialOperator):
     def __init__(self):
         super(GradientOperator, self).__init__()
 
-        self.px = (1, 1, 0, 0, 0, 0)  # paddings
-        self.py = (0, 0, 1, 1, 0, 0)
-        self.pz = (0, 0, 0, 0, 1, 1)
+        # paddings
+        self.px = (0, 1, 0, 0, 0, 0)
+        self.py = (0, 0, 0, 1, 0, 0)
+        self.pz = (0, 0, 0, 0, 0, 1)
 
+        # F.grid_sample(..) takes values in range (-1, 1), so needed for det(J) = 1 when the transformation is identity
         self.pixel_spacing = None
 
     def forward(self, v):
-        # F.grid_sample(..) takes values in range (-1, 1), so needed for det(J) = 1 when the transformation is identity
         if self.pixel_spacing is None:
             dim_x = v.size()[4]
             dim_y = v.size()[3]
@@ -37,14 +38,10 @@ class GradientOperator(DifferentialOperator):
 
             self.pixel_spacing = (2.0 / float(dim_x - 1), 2.0 / float(dim_y - 1), 2.0 / float(dim_z - 1))
 
-        v_padded_x = F.pad(v, self.px, mode='replicate')
-        dv_dx = (v_padded_x[:, :, :, :, 2:] - v_padded_x[:, :, :, :, :-2]) / (2.0 * self.pixel_spacing[0])
-
-        v_padded_y = F.pad(v, self.py, mode='replicate')
-        dv_dy = (v_padded_y[:, :, :, 2:] - v_padded_y[:, :, :, :-2]) / (2.0 * self.pixel_spacing[1])
-
-        v_padded_z = F.pad(v, self.pz, mode='replicate')
-        dv_dz = (v_padded_z[:, :, 2:] - v_padded_z[:, :, :-2]) / (2.0 * self.pixel_spacing[2])
+        # forward differences--hard to say why but it's the only scheme that works..
+        dv_dx = F.pad(v[:, :, :, :, 1:] - v[:, :, :, :, :-1], self.px, mode='replicate') / self.pixel_spacing[0]
+        dv_dy = F.pad(v[:, :, :, 1:] - v[:, :, :, :-1], self.py, mode='replicate') / self.pixel_spacing[1]
+        dv_dz = F.pad(v[:, :, 1:] - v[:, :, :-1], self.pz, mode='replicate') / self.pixel_spacing[2]
 
         nabla_vx = torch.stack((dv_dx[:, 0], dv_dy[:, 0], dv_dz[:, 0]), 1)
         nabla_vy = torch.stack((dv_dx[:, 1], dv_dy[:, 1], dv_dz[:, 1]), 1)
