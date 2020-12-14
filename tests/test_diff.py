utils/diff_op.py: Fix calculating the transformation Jacobian
utils/diff_op.py: Fix calculating the transformation Jacobian
tests/test_diff.py: Fix calculating the transformation Jacobian

tests/test_diff.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 import torch
 
-from utils import calc_det_J, init_identity_grid_3D, pixel_to_normalised_3D, GradientOperator
+from utils import GradientOperator, calc_det_J, init_identity_grid_3D
 
 # fix random seeds for reproducibility
 SEED = 123
@@ -99,10 +99,8 @@ class DiffTestMethods(unittest.TestCase):
         for idx_z in range(v.shape[2]):
             for idx_y in range(v.shape[3]):
                 for idx_x in range(v.shape[4]):
-                    x, y, z = pixel_to_normalised_3D(idx_x, idx_y, idx_z, v.shape[4], v.shape[3], v.shape[2])
-
-                    v[0, 0, idx_z, idx_y, idx_x] = x
-                    v[0, 1, idx_z, idx_y, idx_x] = 1.5 * y + 3.0 * z + 1.0
+                    v[0, 0, idx_z, idx_y, idx_x] = idx_x
+                    v[0, 1, idx_z, idx_y, idx_x] = 1.5 * idx_y + 3.0 * idx_z + 1.0
                     v[0, 2, idx_z, idx_y, idx_x] = 0.0
 
         """
@@ -216,7 +214,7 @@ class DiffTestMethods(unittest.TestCase):
         calculate its Jacobian
         """
 
-        nabla = self.diff_op(identity_transformation)
+        nabla = self.diff_op(identity_transformation, transformation=True)
 
         """
         calculate the log determinant of the Jacobian
@@ -261,7 +259,7 @@ class DiffTestMethods(unittest.TestCase):
         calculate its Jacobian
         """
 
-        nabla = self.diff_op(transformation)
+        nabla = self.diff_op(transformation, transformation=True)
 
         log_det_J = torch.log(calc_det_J(nabla))
         log_det_J_true = torch.log(8.0 * torch.ones_like(log_det_J))

utils/diff_op.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -70,18 +71,23 @@ class GradientOperator(DifferentialOperator):
         # F.grid_sample(..) takes values in range (-1, 1), so needed for det(J) = 1 when the transformation is identity
         self.pixel_spacing = None
 
-    def forward(self, v):
-        if self.pixel_spacing is None:
-            dim_x = v.shape[4]
-            dim_y = v.shape[3]
-            dim_z = v.shape[2]
+    def _set_spacing(self, field):
+        dims = np.asarray(field.shape[2:])
+        self.pixel_spacing = 2.0 / (dims - 1)
 
-            self.pixel_spacing = (2.0 / float(dim_x - 1), 2.0 / float(dim_y - 1), 2.0 / float(dim_z - 1))
+    def forward(self, v, transformation=False):
+        if transformation and self.pixel_spacing is None:
+            self._set_spacing(v)
 
         # forward differences
-        dv_dx = F.pad(v[:, :, :, :, 1:] - v[:, :, :, :, :-1], self.px, mode='replicate') / self.pixel_spacing[0]
-        dv_dy = F.pad(v[:, :, :, 1:] - v[:, :, :, :-1], self.py, mode='replicate') / self.pixel_spacing[1]
-        dv_dz = F.pad(v[:, :, 1:] - v[:, :, :-1], self.pz, mode='replicate') / self.pixel_spacing[2]
+        dv_dx = F.pad(v[:, :, :, :, 1:] - v[:, :, :, :, :-1], self.px, mode='replicate')
+        dv_dy = F.pad(v[:, :, :, 1:] - v[:, :, :, :-1], self.py, mode='replicate')
+        dv_dz = F.pad(v[:, :, 1:] - v[:, :, :-1], self.pz, mode='replicate')
+
+        if transformation:
+            dv_dx /= self.pixel_spacing[2]
+            dv_dy /= self.pixel_spacing[1]
+            dv_dz /= self.pixel_spacing[0]
 
         nabla_vx = torch.stack((dv_dx[:, 0], dv_dy[:, 0], dv_dz[:, 0]), 1)
         nabla_vy = torch.stack((dv_dx[:, 1], dv_dy[:, 1], dv_dz[:, 1]), 1)