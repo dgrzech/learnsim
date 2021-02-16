b44edcb6ba9a7d16f280bb500178dfa2b1b0254f
dgrzech <d.grzech17@imperial.ac.uk>
2021-02-16 22:02:49 +0000

    base/base_trainer.py: Fix an error when lowering the number of iterations after epoch 2

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -107,7 +107,7 @@ class BaseTrainer:
 
         for epoch in range(self.start_epoch, self.no_epochs+1):
             self._train_epoch(epoch)
-            self.no_iters_q_v = self.no_iters_q_v / 2 if epoch == 3 else self.no_iters_q_v
+            self.no_iters_q_v = self.no_iters_q_v // 2 if epoch == 2 else self.no_iters_q_v
 
     def test(self):
         """
