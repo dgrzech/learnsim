110c92aea51c9d1cf43ef9e6af0d1fc6ed92b477
dgrzech <d.grzech17@imperial.ac.uk>
2021-03-03 23:08:46 +0000

    base/base_data_loader.py: Fix the sampler

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -8,8 +8,7 @@ class BaseDataLoader(DataLoader):
     """
 
     def __init__(self, batch_size, dataset, no_GPUs, num_workers, shuffle, rank):
-        init_kwargs = {'batch_size': batch_size, 'dataset': dataset, 'num_workers': num_workers, 'pin_memory': True,
-                       'shuffle': shuffle}
-
+        init_kwargs = {'batch_size': batch_size, 'dataset': dataset, 'num_workers': num_workers, 'pin_memory': True}
         sampler = DistributedSampler(dataset, num_replicas=no_GPUs, rank=rank, shuffle=shuffle) if no_GPUs > 1 else None
+
         super().__init__(sampler=sampler, **init_kwargs)
