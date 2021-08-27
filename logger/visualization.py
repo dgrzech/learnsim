0757cc4c0b9e301486beec7adbc5ab2930f2c529
dgrzech <d.grzech17@imperial.ac.uk>
2021-08-27 12:00:56 +0100

    Fix tensorboard

--- logger/visualization.py
+++ logger/visualization.py
@@ -39,11 +39,9 @@ def log_model_samples(writer, output_dict):
     if dist.get_rank() == 0:
         body_axes = ['coronal', 'axial', 'sagittal']
 
-        positive_samples_slices = get_im_or_field_mid_slices(output_dict['samples_plus_mean'])
         negative_samples_slices = get_im_or_field_mid_slices(output_dict['samples_minus_mean'])
 
         for slice_idx, body_axis_name in enumerate(body_axes):
-            writer.add_images(f'samples_plus_mean/{body_axis_name}', rescale_im_intensity(positive_samples_slices[slice_idx]))
             writer.add_images(f'samples_minus_mean/{body_axis_name}', rescale_im_intensity(negative_samples_slices[slice_idx]))
 
 
